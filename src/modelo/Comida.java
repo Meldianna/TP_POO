7f6ef1f2c031d57d4bb4be2ef58a2c50cf509e67
Meldianna <melrguez05@gmail.com>
2025-06-12 15:53:20 -0300

    Update Comida.java

--- src/modelo/Comida.java
+++ src/modelo/Comida.java
@@ -4,28 +4,49 @@ import java.awt.Image;
 import java.awt.Rectangle;
 
 import interfaces.IDibujable;
-public class Comida extends Dibujo implements IDibujable {
-	private int velocidadX;
-	
+public abstract class Comida extends Dibujo implements IDibujable {
 
-	    public Comida(int posEjeX, int posEjeY, int ancho, int alto, Image imagen) {
-	        super(posEjeX, posEjeY, ancho, alto, imagen);
-	        this.velocidadX = velocidadX;
+		private boolean isVisible;
+		
+	   
+
+		public Comida(int posEjeX, int posEjeY, int ancho, int alto, Image imagen) {
+	        super(posEjeX, posEjeY, ancho, alto, imagen);  
+	        this.isVisible = false;
 	    }
 
 	    @Override
 	    public void dibujar(Graphics g) {
-	        g.drawImage(imagen, posEjeX, posEjeY, anchura, altura, null);
+	        if (isVisible) {
+	            g.drawImage(imagen, posEjeX, posEjeY, anchura, altura, null);
+	        }
 	    }
+		
+	  //cuando este método se invoca, quiere decir que la manzana es consumida
+	    public abstract void aplicarEfecto(GamePanel gp); //tipo objeto porque cada manzana modifica uno distinto
 	    
-	   
+	    
+	    
 	    public void setPosicion(int x, int y) {
 	        this.posEjeX = x;
 	        this.posEjeY = y;
 	    }
 
-	    
+	    public boolean isVisible() {
+			return isVisible;
+		}
+
+		public void setVisible(boolean isVisible) {
+			this.isVisible = isVisible;
+		}
+
+		public Rectangle getBounds() {
+			
+				return new Rectangle(posEjeX, posEjeY, anchura, altura);
+		}
 	}
+
+
 
 
 
