6361ba43b3f67afba1f36a96dcd9152866a29e6d
Meldianna <melrguez05@gmail.com>
2025-06-11 21:03:43 -0300

    Update Background.java

--- src/modelo/Background.java
+++ src/modelo/Background.java
@@ -1,29 +1,9 @@
 package modelo;
 
 import java.awt.Image;
+
 import java.awt.Graphics;
-<<<<<<< HEAD
 
-import interfaces.IDibujable;
-
-public class Background extends Dibujo implements IDibujable{
-
-	
-
-	public Background(int posEjeX, int posEjeY, int anchura, int altura, Image imagen) {
-		super(0, 0, anchura, altura, imagen); ///posEjeX y posEjeY se setean en la coordenada (0,0) de la pantalla
-		///es decir, la imagen se ubica en la coordenada de pixeles (0,0). 
-		this.altura = altura;
-		this.anchura = anchura;
-		this.imagen = imagen;
-	}
-
-	@Override
-    public void dibujar(Graphics g) {
-        g.drawImage(imagen, posEjeX, posEjeY, anchura, altura, null);
-    }
-=======
->>>>>>> dc424ba (juego refactorizado y funcional)
 
 import interfaces.IDibujable;
 
